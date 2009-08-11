36cbd49c280a1ec005c80805e704f72129af7c31
albatrocity <albatrocity@gmail.com>
2009-08-11 15:42:10 -0500

    portfolio_item layout. My associate details.

---
+++ javascripts/post_portfolio_slider.js
@@ -0,0 +1,16 @@
+$(function() {		
+	$("div.scrollable").scrollable({
+		interval: 8000,
+		size: 1,
+		loop: true, 
+		speed: 1000,
+		onBeforeSeek: function() {
+			this.getItems().fadeTo(500, .8);		
+		},
+		onSeek: function() {
+			this.getItems().fadeTo(500, 1);
+		}
+	});	
+});
+
+
